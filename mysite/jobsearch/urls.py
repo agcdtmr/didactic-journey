082e32d44d8f33cf7f5ed848c53fff5f913f7e5b
adtmr <anjdeitmer@gmail.com>
2022-11-28 17:19:58 +0100

    Update urls.py

--- mysite/jobsearch/urls.py
+++ mysite/jobsearch/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.base, name="base"),
-    path("", views.home, name="home"),
-    path("", views.filter_jobs_form, name="filter_jobs_form")
+    path("home", views.home, name="home"),
+    path("filter_jobs_form", views.filter_jobs_form, name="filter_jobs_form"),
+    path("search_results", views.search_results, name="search_results")
 ]

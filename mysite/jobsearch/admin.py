064dbcfcfa67aca1b516f65dd76e29ca3d5bcb32
adtmr <anjdeitmer@gmail.com>
2022-11-28 14:36:45 +0100

    Update admin.py

--- mysite/jobsearch/admin.py
+++ mysite/jobsearch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import JobsSearchDB, Users, SavedJobs
 
 # Register your models here.
+admin.site.register(JobsSearchDB)
+admin.site.register(Users)
+admin.site.register(SavedJobs)
